0e51a3068751fa41baf83797e3183def80837f8c
Thomas BARRAS <thomas.barras26@gmail.com>
2016-11-01 11:46:14 -0400

    chapter1 question3 java naive

--- Java/c1/Q3.java
+++ Java/c1/Q3.java
@@ -3,6 +3,26 @@
 */
 public class Q3 {
 
+
+    /*
+    * Naive approach
+    * count the number of characters of both words and compare
+    */
+    public static boolean isPermutation(String fst, String snd) {
+        if (fst.length() != snd.length())
+            return false;        
+        java.util.HashMap<Character,Integer> fstMap = new java.util.HashMap<Character,Integer>();
+        java.util.HashMap<Character,Integer> sndMap = new java.util.HashMap<Character,Integer>();
+        /* count the occurence, fst and snd have same length */
+        for (int i = 0; i < fst.length(); i++) {
+            Character fstAt = new Character(fst.charAt(i));
+            Character sndAt = new Character(snd.charAt(i));
+            fstMap.put(fstAt, fstMap.containsKey(fstAt) ? new Integer(fstMap.get(fstAt).intValue() + 1) : new Integer(1));
+            sndMap.put(sndAt, sndMap.containsKey(sndAt) ? new Integer(sndMap.get(sndAt).intValue() + 1) : new Integer(1));
+        }
+        return fstMap.equals(sndMap);
+    }    
+
     public static String sort(String s) {
         char[] tmp = s.toCharArray();
         java.util.Arrays.sort(tmp);
@@ -10,14 +30,7 @@ public class Q3 {
     }
 
     /*
-    * Naive approach
-    */
-    public static boolean isPermutation(String fst, String snd) {
-        return sort(fst).equals(sort(snd));
-    }
-
-    /*
-    * think of the length trick
+    * think of the length of words
     */
     public static boolean isPermutation2(String fst, String snd) {
         if (fst.length() != snd.length())
@@ -29,7 +42,9 @@ public class Q3 {
         String[] fsts = {"", "","he","oh","okko"};
         String[] snds = {"", "a","eh","abcdefgh","koko"};
         for (int i = 0; i < fsts.length; i++) {
-            System.out.println(fsts[i] + " is a permutation of " + snds[i] + " ? " + isPermutation2(fsts[i], snds[i]));
+            System.out.println(fsts[i] + " is a permutation of " + snds[i] + " ? " +
+            isPermutation(fsts[i], snds[i]) + ',' +
+            isPermutation2(fsts[i], snds[i]));
         }
     }
 
--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Cracking the Coding Interview solutions
 ### IDE
 It's important to write theses solutions without IDE. Keep it mind that you won't have one for the phone screen interview.
 
+### Is there any trick I need to know ?
+...yes. 
 
 ### Naive approach / implentation
